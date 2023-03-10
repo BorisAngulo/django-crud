67579d4fb0b05923d43bc526fb8260de0634a2c5
borisanthony123 <borisanthony03@gmail.com>
2023-03-09 20:25:39 -0400

    volviendo a subir

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 #ckeditor
 from ckeditor.fields import RichTextField#crea un campo de texto con mas variedad
 
-# Create your models here.
+# Create your models here. a
 class Post(models.Model):#heredamos para crear un modela de db
     image = models.ImageField(verbose_name='Imagen', upload_to='blog') #con verbose_name es como se mostrara en el editor y con upload_to hacemos que las imganens se guarden el img y carptea blog
     title = models.CharField(max_length=200, verbose_name='Título')
