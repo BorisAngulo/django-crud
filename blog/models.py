8af4e0f6d597d3e8eb55db2eb5dbd0a5a3303b94
borisanthony123 <borisanthony03@gmail.com>
2023-03-09 20:28:05 -0400

    eliminando ckeditor

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-#ckeditor
-from ckeditor.fields import RichTextField#crea un campo de texto con mas variedad
+
 
 # Create your models here. a
 class Post(models.Model):#heredamos para crear un modela de db
     image = models.ImageField(verbose_name='Imagen', upload_to='blog') #con verbose_name es como se mostrara en el editor y con upload_to hacemos que las imganens se guarden el img y carptea blog
     title = models.CharField(max_length=200, verbose_name='Título')
     desc = models.TextField(verbose_name='Descripción')
-    content = RichTextField(verbose_name='Contenido')
+    content = models.TextField(verbose_name='Contenido')
     
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     update =models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
